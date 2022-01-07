2da4b48a3c7a09aacb9c22ca730a3d73f2c00e42
Devender Kumar Pahadia <69049819+devenderpahadia@users.noreply.github.com>
2022-01-07 20:42:15 +0530

    Time: 440 ms (14.97%), Space: 164.1 MB (17.07%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    void solve(vector<int>ip,vector<int>op,int index,int k,vector<vector<int>>&res)
+    void solve(int n,vector<int>op,int index,int k,vector<vector<int>>&res)
     {
         if(op.size()==k)
         {
             res.push_back(op);
             return;
         }
-        if(index>=ip.size()) return;
-        int n = ip.size();
-        for(int i=index;i<n;i++)
+        if(index>n) return;
+        for(int i=index;i<=n;i++)
         {
-            op.push_back(i+1);
-            solve(ip,op,i+1,k,res);
+            op.push_back(i);
+            solve(n,op,i+1,k,res);
             op.pop_back();
         }
         return;
@@ -22,15 +21,10 @@ public:
     {
         
         vector<vector<int>>res;
-        vector<int>ip;
         if(n<k)
             return res;
-        for(int i=1;i<=n;i++)
-        {
-            ip.push_back(i);
-        }
         vector<int>op;
-        solve(ip,op,0,k,res);
+        solve(n,op,1,k,res);
         
         
         return res;
