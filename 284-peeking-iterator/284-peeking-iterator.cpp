86145c4208a9e1fbf34327cec9ffa224c2b70a8b
Devender Kumar Pahadia <69049819+devenderpahadia@users.noreply.github.com>
2022-04-25 16:05:45 +0530

    Time: 4 ms (59.03%), Space: 7.6 MB (20.69%) - LeetHub

---
+++ 284-peeking-iterator/284-peeking-iterator.cpp
@@ -0,0 +1,50 @@
+/*
+ * Below is the interface for Iterator, which is already defined for you.
+ * **DO NOT** modify the interface for Iterator.
+ *
+ *  class Iterator {
+ *		struct Data;
+ * 		Data* data;
+ *  public:
+ *		Iterator(const vector<int>& nums);
+ * 		Iterator(const Iterator& iter);
+ *
+ * 		// Returns the next element in the iteration.
+ *		int next();
+ *
+ *		// Returns true if the iteration has more elements.
+ *		bool hasNext() const;
+ *	};
+ */
+
+class PeekingIterator : public Iterator {
+public:
+    queue<int>q;
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+        for(int i=0;i<nums.size();i++)
+        {
+            q.push(nums[i]);
+        }
+	    
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        return q.front();
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+	    int x=q.front();
+        q.pop();
+        return x;
+	}
+	
+	bool hasNext() const {
+	    return !q.empty();
+	}
+};
