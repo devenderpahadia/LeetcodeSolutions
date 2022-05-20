9bbac28b906cb4d50bac71808e68ce56201d3246
Devender Kumar Pahadia <69049819+devenderpahadia@users.noreply.github.com>
2022-05-20 22:19:45 +0530

    Time: 5 ms (17.71%), Space: 7 MB (99.28%) - LeetHub

---
+++ 140-word-break-ii/140-word-break-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    void solve(int start,string str,string& s,vector<string>&res,unordered_set<string>&st){
+        if(start==s.size()){
+            str.pop_back();
+            res.push_back(str);
+            return;
+        }
+        for(int i=start;i<s.size();i++){
+            string temp = s.substr(start,i-start+1);
+            if(st.find(temp)!=st.end()){
+                temp+=' ';
+                // cout<<temp<<endl;
+                solve(i+1,str+temp,s,res,st);
+            }
+        }
+    }
+    
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string>st;
+        for(auto it : wordDict){
+            st.insert(it);
+        }
+        vector<string>res;
+        for(int i=0;i<s.size();i++){
+            string temp = s.substr(0,i+1);
+            if(st.find(temp)!=st.end()){
+                temp+=' ';
+                solve(i+1,temp,s,res,st);
+            }
+        }
+        return res;
+    }
+};
