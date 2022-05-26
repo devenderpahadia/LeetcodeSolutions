046b9067e6c7fee003b7678d722deae5c737852f
Devender Kumar Pahadia <69049819+devenderpahadia@users.noreply.github.com>
2022-05-26 23:32:22 +0530

    Time: 7 ms (12.48%), Space: 5.8 MB (79.17%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -3,10 +3,8 @@ public:
     int hammingWeight(uint32_t n) 
     {
         int ctr=0;
-        while(n!=0)
-        {
-            ctr+= (n%2);
-            n=n/2;
+        for(int i=31;i>=0;i--){
+            ctr+=((n>>i)&1);
         }
         return ctr;
     }
