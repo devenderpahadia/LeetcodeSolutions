c398c3237ed8c7902a72bafb2e0cfb766bbbda4b
Devender Kumar Pahadia <69049819+devenderpahadia@users.noreply.github.com>
2022-05-30 18:00:16 +0530

    Time: 108 ms (11.06%), Space: 24 MB (40.79%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -3,15 +3,17 @@ public:
     vector<int> nextGreaterElements(vector<int>& arr) {
         int n = arr.size();
         stack<int>st;
-        vector<int>res;
+        vector<int>res(n,-1);
         for(int i=2*n-1;i>=0;i--){
             while(st.size() && st.top()<=arr[i%n]) st.pop();
-            if(st.empty()) res.push_back(-1);
-            if(st.size() && st.top()>arr[i%n]) res.push_back(st.top());
+            if(i<n){
+                if(st.size() && st.top()>arr[i])
+                res[i] = st.top();
+            }
             st.push(arr[i%n]);
         }
-        reverse(res.begin(),res.end());
-        res.resize(n);
+        // reverse(res.begin(),res.end());
+        // res.resize(n);
         return res;
     }
 };
