451fe28d907b2019d5a41a6081b328cd633cca80
Devender Kumar Pahadia <69049819+devenderpahadia@users.noreply.github.com>
2022-06-04 16:05:34 +0530

    Time: 85 ms (10.80%), Space: 50.2 MB (6.97%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
-    void solve(int col,vector<string>mat,vector<int>lrow,vector<int>ldiag,vector<int>udiag,vector<vector<string>>&res){
-        
-        if(col==mat.size()){
-            res.push_back(mat);
+    bool check(int i,int j,vector<string>arr){
+        for(int x = j-1;x>=0;x--){
+            if(arr[i][x]=='Q') return false;
+        }
+        int x = i;
+        int y = j;
+        while(x-- && y--)
+        {
+            if(arr[x][y]=='Q') return false;
+        }
+        x = i, y = j;
+        while(++x < arr.size() && y--)
+        {
+            if(arr[x][y]=='Q') return false;
+        }
+        return true;
+    }
+    void solve(int j,int n,vector<string>vec,vector<vector<string>>&res){
+        if(j==n) {
+            res.push_back(vec);
             return;
         }
-        for(int row=0;row<mat.size();row++){
-            if(lrow[row]==0 && ldiag[row+col]==0 && udiag[mat.size()-1 + (col-row)]==0){
-                mat[row][col] = 'Q';
-                lrow[row]=1;
-                ldiag[row+col]=1;
-                udiag[mat.size()-1 + (col-row)]=1;
-                solve(col+1,mat,lrow,ldiag,udiag,res);
-                mat[row][col] = '.';
-                lrow[row]=0;
-                ldiag[row+col]=0;
-                udiag[mat.size()-1 + (col-row)]=0;
+        for(int i=0;i<n;i++){
+            if(check(i,j,vec)){
+                vec[i][j] = 'Q';
+                solve(j+1,n,vec,res);
+                vec[i][j] = '.';
             }
         }
     }
     
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>>res;
-        vector<string>mat(n,string(n,'.'));
-        vector<int>lrow(n,0);
-        vector<int>ldiag(2*n-1,0);
-        vector<int>udiag(2*n-1,0);
-        solve(0,mat,lrow,ldiag,udiag,res);
-        
+        vector<string>vec(n,string(n,'.'));
+        solve(0,n,vec,res);
         return res;
     }
 };
