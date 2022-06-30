89cabaeb48a3625f47984aae6b42eb4dfb65c05f
Devender Kumar Pahadia <69049819+devenderpahadia@users.noreply.github.com>
2022-07-01 01:21:48 +0530

    Time: 105 ms (69.41%), Space: 10.6 MB (79.51%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
-    int dp[13][10001];
-    int solve(int i,vector<int>& coins, int amount){
-        if(amount == 0){
-            return 0;
-        }
-        if(i<0 || amount<0){
-            return 1e5;
-        }
-        if(dp[i][amount]!=-1) return dp[i][amount];
-        return dp[i][amount]= min(1 + solve(i,coins,amount-coins[i]),solve(i-1,coins,amount));
-    }
     int coinChange(vector<int>& coins, int amount) {
-        memset(dp,-1,sizeof dp);
-        int res = solve(coins.size()-1,coins,amount);
-        return res>=1e5 ? -1:res;
+        
+        int n = coins.size();
+        int t[n+1][amount+1];
+        
+        for(int j=1;j<=amount;j++){
+            t[0][j] = INT_MAX-1;
+        }
+        for(int i=0;i<=n;i++){
+            t[i][0] = 0;
+        }
+        for(int j=1;j<=amount;j++){
+            if(j%coins[0]==0) t[1][j] = j/coins[0];
+            else t[1][j] = INT_MAX-1;
+        }
+        
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(coins[i-1]<=j){
+                    t[i][j] = min(t[i-1][j],1+t[i][j-coins[i-1]]);
+                }
+                else t[i][j] = t[i-1][j];
+            }
+        }
+        return t[n][amount]==INT_MAX-1 ? -1 : t[n][amount];
     }
 };
