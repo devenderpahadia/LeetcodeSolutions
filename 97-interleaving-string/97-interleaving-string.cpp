5e6f3b51897159ee0f400dc5b9c75191c2e17981
Devender Kumar Pahadia <69049819+devenderpahadia@users.noreply.github.com>
2022-07-07 13:46:13 +0530

    Time: 148 ms (5.02%), Space: 34 MB (15.62%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,61 +1,25 @@
 class Solution {
 public:
+    int dp[101][101][201];
     
-    bool solve(int i,string s1,int j,string s2,int k,string s3,unordered_map<string,bool>&mp)
-    {
-        if(k==s3.size())
-            return true;
+    bool solve(int i,string s1,int j,string s2,int k,string s3){
+        if(k==s3.size()) return true;
         
-        string temp = to_string(i)+"_"+to_string(j)+"_"+to_string(k);
-        if(mp.find(temp)!=mp.end())
-                return mp[temp];
+        if(dp[i][j][k]!=-1) return dp[i][j][k];
         
-        bool l = false,r=false;
-        if(i<s1.size())
-        {
-            if(s1[i]==s3[k]){
-                string temp1 = to_string(i+1)+"_"+to_string(j)+"_"+to_string(k+1);
-                if(mp.find(temp1)!=mp.end())
-                    l = mp[temp1];
-                else
-                    l = solve(i+1,s1,j,s2,k+1,s3,mp);
-                    mp[temp1] = l;
-            }
+        
+        if(i<s1.size() && j<s2.size() && s1[i]==s3[k] && s2[j]==s3[k]){
+            return dp[i][j][k] =  solve(i+1,s1,j,s2,k+1,s3) || solve(i,s1,j+1,s2,k+1,s3);
         }
-        if(j<s2.size())
-        {
-            if(s2[j]==s3[k]){
-                string temp2 = to_string(i)+"_"+to_string(j+1)+"_"+to_string(k+1);
-                if(mp.find(temp2)!=mp.end())
-                    r = mp[temp2];
-                else
-                    r = solve(i,s1,j+1,s2,k+1,s3,mp);
-                    mp[temp2] = r;
-            }
-        }
-        return mp[temp] = l || r;
+        if(i<s1.size() && s1[i]==s3[k]) return dp[i][j][k] = solve(i+1,s1,j,s2,k+1,s3);
+        if(j<s2.size() && s2[j]==s3[k]) return dp[i][j][k] = solve(i,s1,j+1,s2,k+1,s3);
+        
+        return dp[i][j][k] = false;
     }
     
     bool isInterleave(string s1, string s2, string s3) {
-        if(s1.size()+s2.size()!=s3.size())
-            return false;
-        if(s1.size()+s2.size()==0)
-            return true;
-        
-//         int x = s1.size();
-//         int y = s2.size();
-//         int z = s3.size();
-//         bool t[x+1][y+1][z+1];
-//         t[0][0] = true;
-        
-//         for(int i=1;i<=x;i++){
-//             if(s1[i-1]==s3[])
-//             t[i][0]=false;
-//         }
-//         for(int i=1;i<=y;i++)
-//             t[0][j]=false;
-        
-        unordered_map<string,bool>mp;
-        return solve(0,s1,0,s2,0,s3,mp);
+        memset(dp,-1,sizeof dp);
+        if(s1.size()+s2.size()!=s3.size()) return false;
+        return solve(0,s1,0,s2,0,s3);
     }
 };
