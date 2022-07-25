4cca46d36ba44ae17026162c43ecd67c33d77f7a
Devender Kumar Pahadia <69049819+devenderpahadia@users.noreply.github.com>
2022-07-25 14:10:33 +0530

    Time: 8 ms (84.21%), Space: 13.8 MB (17.99%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,63 +1,35 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) 
-    {
-        int first=-1;
-        int second=-1;
-        int l=0;
-        int h=nums.size()-1;
-        int mid;
-        
-        while(l<=h)
-        {
-            mid= l+(h-l)/2;
-            if(nums[mid]==target)
-            {
-                first=mid;
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int res1 = -1;
+        int res2 = -1;
+        int n = nums.size();
+        int l = 0;
+        int h = n-1;
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            if(nums[mid]==target){
+                res1 = mid;
                 h=mid-1;
             }
-            else if(target<nums[mid])
-            {
+            else if(nums[mid]>target){
                 h=mid-1;
             }
-            else
-            {
+            else l=mid+1;
+        }
+        l = 0;
+        h = n-1;
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            if(nums[mid]==target){
+                res2 = mid;
                 l=mid+1;
             }
-        }
-        l=0;
-        h=nums.size()-1;
-        while(l<=h)
-        {
-            mid= l+(h-l)/2;
-            if(nums[mid]==target)
-            {
-                second=mid;
-                l=mid+1;;
-            }
-            else if(target<nums[mid])
-            {
+            else if(nums[mid]>target){
                 h=mid-1;
             }
-            else
-            {
-                l=mid+1;
-            }
+            else l=mid+1;
         }
-        //cout<<first<<" "<<second<<endl;
-        vector<int>res;
-        if(first==-1 || second==-1)
-        {
-            res.push_back(-1);
-            res.push_back(-1);
-            return res;
-        }
-        else
-        {
-            res.push_back(first);
-            res.push_back(second);
-            return res;
-        }
-        
+        return {res1,res2};
     }
 };
