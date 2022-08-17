152d8cca4e3ed8eabcce64b7cd84fc6abbdfa3d9
Devender Kumar Pahadia <69049819+devenderpahadia@users.noreply.github.com>
2022-08-17 12:51:37 +0530

    Time: 9 ms (26.15%), Space: 8.8 MB (11.70%) - LeetHub

---
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        vector<string>vec = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        
+        unordered_map<char,string>mp;
+        for(int i=0;i<vec.size();i++){
+            mp[i+'a'] = vec[i];
+        }
+        unordered_set<string>st;
+        for(auto it : words){
+            string str ="";
+            for(auto i : it){
+                str+=(mp[i]);
+            }
+            st.insert(str);
+        }
+        return st.size();
+    }
+};
